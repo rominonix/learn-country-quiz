f1148fdfe379e89d8216589923d8e13aba27bb28
Romina Rebolledo <rominarebolledo@gmail.com>
2021-11-09 15:16:04 +0100

    wip random questions

--- app/src/utils.js
+++ app/src/utils.js
@@ -1,76 +1,112 @@
-import countries from "./countries"
+import countries from "./countries";
 
 const hardCodedQuestions = {
-	1: {
-		alternatives: {
-			1: 'swe',
-			2: 'fra',
-			3: 'dnk',
-			4: 'bra',
-		},
-		correct: 'swe',
-	},
-	2: {
-		alternatives: {
-			1: 'blz',
-			2: 'fra',
-			3: 'cub',
-			4: 'cog',
-		},
-		correct: 'fra',
-	}
-}
+  1: {
+    alternatives: {
+      1: "swe",
+      2: "fra",
+      3: "dnk",
+      4: "bra",
+    },
+    correct: "swe",
+  },
+  2: {
+    alternatives: {
+      1: "blz",
+      2: "fra",
+      3: "cub",
+      4: "cog",
+    },
+    correct: "fra",
+  },
+};
 // we must to create a new feature flag for this new function
 // we need to create a test with mocha for to have control if one's of country are repeat in the same question
 
 // function for to convert keys of the countries Obj to lowercase
 const country = Object.keys(countries).reduce(
-	(prev, current) => ({
-	  ...prev,
-	  [current.toLowerCase()]: countries[current],
-	}),
-	{}
-  );
- console.log(country); 
+  (prev, current) => ({
+    ...prev,
+    [current.toLowerCase()]: countries[current],
+  }),
+  {}
+);
 
-// for to take keys of the country object and convert to array  
+// for to take keys of the country object and convert to array
 const arrCountries = Object.keys(country);
-console.log(arrCountries);
-
 
 // loop for get four random countries and save in the arrRandomCountry variable
-const arrRandomCountry = []
+const arrRandomCountry = [];
 
 while (arrCountries.length > 212) {
-	let randomCountry = Math.floor(Math.random() * arrCountries.length)
-	let pushRandomCountry = arrCountries.splice(randomCountry, 1)
-	arrRandomCountry.push(pushRandomCountry[0])	  
+  let randomCountry = Math.floor(Math.random() * arrCountries.length);
+  let pushRandomCountry = arrCountries.splice(randomCountry, 1);
+  arrRandomCountry.push(pushRandomCountry[0]);
 }
 
-console.log(arrRandomCountry);
+const randomQuestionsGame = {
+  1: {
+    alternatives: {
+      1: arrRandomCountry[0],
+      2: arrRandomCountry[1],
+      3: arrRandomCountry[2],
+      4: arrRandomCountry[3],
+    },
+    correct: arrRandomCountry[2]
+  },
 
-// const randomQuestions = {
+  2: {
+    alternatives: {
+      1: arrRandomCountry[0],
+      2: arrRandomCountry[1],
+      3: arrRandomCountry[2],
+      4: arrRandomCountry[3],
+    },
+    correct: arrRandomCountry[1]
+  },
+  3: {
+    alternatives: {
+      1: arrRandomCountry[0],
+      2: arrRandomCountry[1],
+      3: arrRandomCountry[2],
+      4: arrRandomCountry[3],
+    },
+    correct: arrRandomCountry[3]
+  },
+  4: {
+    alternatives: {
+      1: arrRandomCountry[0],
+      2: arrRandomCountry[1],
+      3: arrRandomCountry[2],
+      4: arrRandomCountry[3],
+    },
+    correct: arrRandomCountry[0]
+  },
+  5: {
+    alternatives: {
+      1: arrRandomCountry[0],
+      2: arrRandomCountry[1],
+      3: arrRandomCountry[2],
+      4: arrRandomCountry[3],
+    },
+    correct: arrRandomCountry[1]
+  },
+};
 
-	
-// 	question: {
-// 		alternatives: {
-// 			1: randomCountry[0],
-// 			2:randomCountry[1],
-// 			3:randomCountry[2],
-// 			4:randomCountry[3]
-// 		}
-// 	}
-// }
+console.log(randomQuestionsGame);
 
+let { randomQuestions } = JSON.parse(localStorage.getItem("featureFlags"));
+let randomQuestionsValue = randomQuestions.value;
 export const createGame = () => {
-	const generatedQuestions = hardCodedQuestions
-	return {
-		currentQuestion: 1,
-		questions: generatedQuestions,
-		score: {player1: 0, player2: 0},
-		status: 'starting',
-	}
-}
+  const generatedQuestions = randomQuestionsValue
+    ? randomQuestionsGame
+    : hardCodedQuestions;
+  return {
+    currentQuestion: 1,
+    questions: generatedQuestions,
+    score: { player1: 0, player2: 0 },
+    status: "starting",
+  };
+};
 
-
-export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
